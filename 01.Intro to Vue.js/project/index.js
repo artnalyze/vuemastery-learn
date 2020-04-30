61873529b13f0c2721fb3afad512604c3fbe3b1f
Artnalyze <artnalyze@gmail.com>
2020-04-30 13:36:18 +0700

    01.Intro to Vue.js : 02 Attribute Binding - v-bind

--- 01.Intro to Vue.js/project/index.js
+++ 01.Intro to Vue.js/project/index.js
@@ -4,7 +4,9 @@ var app = new Vue({
     el: '#app',
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/vmSocks-green.jpg',
+            altText: "A pair of socks"
         }
     }
 })
